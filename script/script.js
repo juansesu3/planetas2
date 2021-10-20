a306379e273f177f0af8b8fc2dd3347232f55b0e
juansesu3 <juanseu3@hotmail.com>
2021-10-20 13:50:50 -0500

    ultimos archivos

---
+++ script/script.js
@@ -0,0 +1,42 @@
+let nombrePlanetas = Array ('Tierra', 'Marte', 'jupiter');
+let distancia = [6,3,'67'];
+const tamaño = []
+
+console.log(nombrePlanetas);
+console.log(distancia);
+console.log(tamaño);
+
+let i=0;
+while(i <= nombrePlanetas.length -1){
+    console.log(i)
+    console.log(nombrePlanetas[i]);
+    i = i + 1    
+}
+for (let index = 0; index < nombrePlanetas.length; index++){
+    console.log(nombrePlanetas[index]);
+}
+for (const key in object){
+    if(Object.hasOwmProperty.call(object, key)){
+        const element = object[key];
+    }
+}
+for (const key in nombrePlanetas){
+    console.log(nombrePlanetas[key])
+}
+for (const planeta of nombrePlanetas){
+    console.log(planeta)
+}
+for (const key in distancia){
+    if(distancia[key] == '5'){
+        console.log('Se encontro la distacia')
+    }else{
+        console.log('No se encontro la distancia')
+    }
+}
+for (const key in distancia){
+    if(distancia[key] === '5'){
+        console.log('Se encontro la distacia')
+    }else{
+        console.log('No se encontro la distancia')
+    }
+}
